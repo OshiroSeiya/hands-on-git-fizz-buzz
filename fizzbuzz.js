9f99e1aa64007281fc77a29b442001015444f94f
oshiro_seiya <oshiro_seiya@ca-adv.co.jp>
2018-11-09 16:27:22 +0900

    5で割り切れるときはBuzzにした
    すでに3で割り切れてるときにBuzzを表示できないのは困るためoutput変数おいてとりあえず成約クリアできるようにした。リファクタはいつかやる

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -2,9 +2,19 @@ var from = 1;
 var to = 100;
 
 for (var current = from; current <= to; current++) {
+  var output = '';
+
   if (current % 3 === 0) {
-    console.log('Fizz');
-  } else{
-    console.log(current);
+    output = 'Fizz';
   }
+
+  if (current % 5 === 0) {
+    // すでにFizzが入ってる可能性があるので文字列結合する
+    output += 'Buzz';
+  }
+
+  if (output === '') {
+    output = current;
+  }
+  console.log(output);
 }
